ce4e2027fc3d9a8a3abef910d6528f395f1e3668
Admin <admin@Intern3-MacBook-Pro.local>
2019-02-20 12:55:09 +0530

    Add Backend

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 import psycopg2
 from flask import Flask, render_template, request
 from flask_cors import CORS
-from modules.post_details import post_data
+# from modules.post_details import post_data
+
 
 app = Flask(__name__)
 CORS(app)
-connection = psycopg2.connect("dbname=officecafe user=admin")
+connection = psycopg2.connect("dbname = officecafe  user=admin")
 
 
 @app.route('/')
--- app_first.py
+++ app_first.py
@@ -1,11 +1,21 @@
-from flask import Flask, render_template
+import psycopg2
+from flask import Flask, render_template, request
+from flask_cors import CORS
+from modules.post_details import post_data
 
 app = Flask(__name__)
+CORS(app)
+connection = psycopg2.connect("dbname=officecafe user=admin")
 
 
 @app.route('/')
 def index():
     return render_template('first_page.html')
+
+
+# @app.route('/first_page.html')
+# def get_data():
+#     return render_template('second_page.html')
 
 
 if __name__ == '__main__':
