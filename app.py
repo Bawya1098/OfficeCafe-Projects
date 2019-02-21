fa91817fb01879a5bb81516a8a9bb00dca7e0a94
Admin <admin@Intern3-MacBook-Pro.local>
2019-02-21 17:10:55 +0530

    add link_to_Flask

--- app.py
+++ app.py
@@ -21,4 +21,4 @@ def get_data():
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
--- app_first.py
+++ app_first.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template, request
 from flask_cors import CORS
 
-
-app = Flask(__name__, static_folder="/Users/admin/OfficeCafe-UI-FrontEnd/static/Images")
+app = Flask(__name__)
 
 CORS(app)
+
+
 @app.route('/')
 def index():
     return render_template('first_page.html')
@@ -14,15 +15,15 @@ def index():
 def second_page():
     return render_template('second_page.html')
 
+
 @app.route('/cafe2')
 def third_page():
-    return render_template('third_page.html')
+    return render_template('third_page_mc.html')
+
 
 @app.route('/cafe3')
 def third_page1():
     return render_template('third_page.html')
-
-
 
 
 if __name__ == '__main__':
