d24c5843d1e61303514d77b1bc093da964b9ba69
Admin <admin@Intern3-MacBook-Pro.local>
2019-03-04 09:26:27 +0530

    add link with database

--- Final.py
+++ Final.py
@@ -6,7 +6,6 @@ app = Flask(__name__)
 CORS(app)
 connection = psycopg2.connect("dbname = officecafe user=admin")
 
-
 app = Flask(__name__)
 
 CORS(app)
@@ -44,7 +43,7 @@ def vendor_login():
 
 def validate_data(connection, user_data):
     cursor = connection.cursor()
-    cursor.execute("select vendor_id from vendor_details where  vendor_id =%(id)s",
+    cursor.execute("select * from vendor_details where  vendor_id =%(id)s and shop =1 or shop = 2 ",
                    {'id': user_data['id']})
     returned_rows = cursor.fetchall()
     cursor.close()
@@ -60,19 +59,14 @@ def juices():
     items = []
     for row in list:
         items.append(row[0])
-    return render_template("check_box_juices.html", items=items)
+    return render_template("available_cold_beverages.html", items=items)
 
 
 def database_connect_function():
-    try:
-        connection = psycopg2.connect(user="admin", host="127.0.0.1", port="5432",
-                                      database="officecafe")
-        cursor = connection.cursor()
-        cursor.execute("select available_juices from list_juices")
-        record = cursor.fetchall()
-        return record
-    except (Exception, psycopg2.Error) as error:
-        print("Error in connecting to PSQL", error)
+    cursor = connection.cursor()
+    cursor.execute("select items_name from items ")
+    record = cursor.fetchall()
+    return record
 
 
 @app.route('/submission', methods=['POST'])
@@ -83,10 +77,8 @@ def menu_list_juices():
 def database_connection_list_cold(connection, user_data):
     cursor = connection.cursor()
     array = tuple(user_data.keys())
-    sql_query = "update  list_juices set checkbox_yes_no = ' '"
-    sql_query_yes = "update  list_juices set checkbox_yes_no = 'yes' WHERE available_juices IN %s"
-    cursor.execute(sql_query)
-    cursor.execute(sql_query_yes, (array,))
+    sql_query_yes = "update  items set is_available = 't' WHERE  items_name = %s"
+    cursor.execute(sql_query_yes, array)
     connection.commit()
     cursor.close()
 
